44478334e0ec189823ee3737568c811b44bc5f2f
Tanmai Raghava <tanmairaghav3836@gmail.com>
2025-06-08 17:41:23 +0530

    checkmate

--- frontend/src/components/fooddisplay/FoodDisplay.jsx
+++ frontend/src/components/fooddisplay/FoodDisplay.jsx
@@ -3,11 +3,10 @@ import "./FoodDisplay.css"
 import FoodItem from '../fooditem/FoodItem'
 import { StoreContext } from '../../context/Storecontext'
 
-const FoodDisplay = ({category}) => {
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
 
-    const {food_list} = useContext(StoreContext)
-
-    const filteredList = category === "All"
+  const filteredList = category === "All"
     ? food_list
     : food_list.filter(item => item.category === category);
 
@@ -15,12 +14,19 @@ const FoodDisplay = ({category}) => {
     <div className='food-display' id="food-display">
       <h2>Top Dishes !</h2>
       <div className="food-display-list">
-        {food_list.map((item) => {
-            return <FoodItem key={item._id} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
-        })}
+        {filteredList.map((item) => (
+          <FoodItem 
+            key={item._id} 
+            id={item._id} 
+            name={item.name} 
+            description={item.description} 
+            price={item.price} 
+            image={item.image} 
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default FoodDisplay
--- frontend/src/components/fooditem/FoodItem.jsx
+++ frontend/src/components/fooditem/FoodItem.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import { assets } from '../../assets/assets'
 import "./FoodItem.css"
 
-const FoodItem = ({ id, name, price, description, image }) => {
+const FoodItem = ({ id, name, description, price, image }) => {
   return (
     <div className='food-item'>
-        <div className="food-item-image-container">
-            <img src={image} alt={img} className="food-item-image" />
+      <div className="food-item-image-container">
+        <img src={image} alt={name} className="food-item-image" />
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="Rating Stars" />
         </div>
-        <div className="food-item-info">
-            <div className="food-item-name-rating">
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt="" />
-            </div>
-            <p className="food-item-disc">{description}</p>
-            <p className="food-item-price">{price}</p>
-        </div>
+        <p className="food-item-disc">{description}</p>
+        <p className="food-item-price">{price}</p>
+      </div>
     </div>
   )
 }
